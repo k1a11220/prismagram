ec3945e46a1efe0492bb823932d47381450ab632
Son Beomsoo <k1a11220@naver.com>
2020-04-12 17:14:28 +0900

    Mailgun error_1
    Error: Error: Forbidden 에러 발생

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
-require("dotenv").config();
-import { GraphQLServer } from "graphql-yoga";
-import logger from "morgan";
-import schema from "./schema";
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+import { sendSecretMail } from './utils';
+
+sendSecretMail('k1a11220@naver.com', '123');
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
 
-server.express.use(logger("dev"));
+server.express.use(logger('dev'));
 
-server.start({ port: PORT }, () =>
-  console.log(`Server running on port http://localhost:${PORT}`)
-);
+server.start({ port: 4000 }, () => console.log(`Server running on port http://localhost:${PORT}`));
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,40 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import mg from 'nodemailer-mailgun-transport';
 
 export const generateSecret = () => {
 	const randomNumber = Math.floor(Math.random() * adjectives.length);
 	return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = email => {
+	const options = {
+		auth: {
+			api_key: process.env.MAILGUN_API,
+			domain: process.env.MAILGUN_DOMAIN
+		}
+	};
+
+	const nodemailerMailgun = nodemailer.createTransport(mg(options));
+	return nodemailerMailgun.sendMail(email, (err, info) => {
+		if (err) {
+			console.log(`Error: ${err}`);
+		} else {
+			console.log(`Response: ${info}`);
+		}
+	});
+};
+
+export const sendSecretMail = (address, secret) => {
+	const email = {
+		from: 'k1a11220@naver.com',
+		to: address,
+		subject: '🔒Login Secret for Prismagram🔒',
+		html: `Hello! Your login secret is ${secret}.
+Copy paste on the app/website to log in`
+	};
+	return sendMail(email);
 };
